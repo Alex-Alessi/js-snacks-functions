812c43988b848ce01ed9553ac0b75550f8f94bc4
Alex-Alessi <alex.alessi246@gmail.com>
2024-10-21 15:13:10 +0200

    bonus complete

--- 05 - bonus/script.js
+++ 05 - bonus/script.js
@@ -5,14 +5,26 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+//! Siccome tra la sera e il mattino non abbiamo un intervallo, metto che dalle 17 alle 5 (orario dell'alba circa) dia buonasera.
 
+const name = "Mario";
 
 // Dichiara la funzione qui.
+const saluto = (nome) => {
+  let data = new Date();
+  let ora = data.getHours();
+  if (ora >= 5 && ora <= 13) {
+    return "Buongiorno " + nome;
+  } else if (ora > 13 && ora <= 17) {
+    return "Buon Pomeriggio " + nome;
+  } else {
+    return "Buonasera " + nome;
+  }
+};
 
+const result = saluto(name);
+console.log(result);
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
