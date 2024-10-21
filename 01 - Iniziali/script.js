4f1dd52dd99622265322cbefd28f3ac6b13b1b08
Alex-Alessi <alex.alessi246@gmail.com>
2024-10-21 14:08:17 +0200

    ex1 complete

--- 01 - Iniziali/script.js
+++ 01 - Iniziali/script.js
@@ -1,13 +1,16 @@
 /* Scrivi una funzione che accetti un array di nomi e restituisca nuovo un array con le iniziali di ogni parola dell'array fornito */
 
 const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
-
+let letters = [];
 
 // Dichiara la funzione qui.
-
+const firstLetter = (parola) => parola[0];
 
 // Invoca la funzione qui e stampa il risultato in console
+for (let i = 0; i < names.length; i++) {
+  letters.push(firstLetter(names[i]));
+}
 
-
+console.log(letters);
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
