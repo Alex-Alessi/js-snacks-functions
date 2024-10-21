5787aebed6b716955538a1323c195716a9807de4
Alex-Alessi <alex.alessi246@gmail.com>
2024-10-21 15:00:46 +0200

    ex 3/4 complete

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,23 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
 
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
 
+const numVowels = (stringa) => {
+  const vowels = ["a", "A", "e", "E", "i", "I", "o", "O", "u", "U"];
+  let counter = 0;
+  for (let i = 0; i < stringa.length; i++) {
+    if (vowels.includes(stringa[i])) {
+      counter++;
+    }
+  }
+  return counter;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const result = numVowels(word);
+console.log(result);
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,14 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
 
+const saluto = (stringa) => "Ciao " + stringa;
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+const result = saluto(name);
+console.log(result);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
